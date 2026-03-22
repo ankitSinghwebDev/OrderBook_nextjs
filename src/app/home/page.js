0e41ccdad09f30aaa6c0bb546f4b9047aae60ab8
ankitSinghwebDev <ankitsingh.webdev@gmail.com>
2026-03-22 23:17:48 +0530

    ADD

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,8 @@
 import { Suspense } from 'react';
 import HomeContent from './HomeContent';
 
+export const dynamic = 'force-dynamic';
+
 export default function HomePage() {
   return (
     <Suspense fallback={<div className="flex justify-center py-20"><div className="page-spinner" /></div>}>
